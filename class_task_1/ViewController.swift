5fe8037c4ddc2bcdd01f93305c14c5ff50ba323b
nuras <nogayev.nuras@gmail.com>
2021-02-16 16:42:43 +0300

    Initial Commit

---
+++ class_task_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  class_task_1
+//
+//  Created by Admin on 16.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
